c2dc88cf7df0278d7703261289ac52eaec93097a
Auzelg <59143910+Auzelg@users.noreply.github.com>
2020-08-01 09:09:25 +0200

    Added Authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
@@ -18,10 +19,11 @@ function App() {
         >
           Learn React
         </a>
-        <h1>Hello World!</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
